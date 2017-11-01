4fda0a50f0bc526a46b1ae9fa9ac192d7b9497c2
Jorge Vilches Gutierrez <jvg637@gmail.com>
2017-11-01 18:59:28 +0100

    (X) Ejercicio: Creación de la aplicación MasterListas (X) Ejercicio: Preparar gráficos para diferentes resoluciones (X) Ejercicio: Elegir la paleta de colores de la aplicación (X) Ejercicio: Crear superficies (X) Ejercicio: Añadiendo un botón flotante (X) Ejercicio: Mostrar un mensaje con Snackbar (X) Ejercicio: Rompimiento de bordes con el botón (X) Ejercicio: Vista de scroll (X) Ejercicio: Imagen que se colapsa (X) Ejercicio: RecyclerView para mostrar las listas (X) Ejercicio: Uso de CardView (X) Ejercicio: Selección de un elemento en un RecyclerView (X) Ejercicio: Creación de la pantalla de inicio de sesión (X) Ejercicio: Uso de Checkbox (X) Ejercicio: Mostrar/ocultar caracteres en campo de contraseña (X) Ejercicio: Botones con efecto onda (ripple) (X) Ejercicio: Crear botones con iconos (X) Ejercicio: Crear botones planos

--- app/src/main/java/org/example/masterlistas/ListasActivity.java
+++ app/src/main/java/org/example/masterlistas/ListasActivity.java
@@ -1,13 +1,21 @@
 package org.example.masterlistas;
 
-import android.app.Activity;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListasActivity extends AppCompatActivity {
+    private RecyclerView recycler;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager lManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +29,29 @@ public class ListasActivity extends AppCompatActivity {
                 Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG).show();
             }
         });
+
+        //Inicializar los elementos
+        List items = new ArrayList();
+        items.add(new Lista(R.drawable.trabajo, "Trabajo", 2));
+        items.add(new Lista(R.drawable.casa, "Personal", 3));
+        // Obtener el Recycler
+        recycler = (RecyclerView) findViewById(R.id.reciclador);
+        recycler.setHasFixedSize(true);
+        // Usar un administrador para LinearLayout
+        lManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(lManager);
+        // Crear un nuevo adaptador
+        adapter = new ListaAdapter(items);
+        recycler.setAdapter(adapter);
+
+        recycler.addOnItemTouchListener(
+                new RecyclerItemClickListener(ListasActivity.this, new
+                        RecyclerItemClickListener.OnItemClickListener() {
+                            @Override public void onItemClick(View v, int position) {
+                                Toast.makeText(ListasActivity.this, "" + position,
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+        );
     }
 }
